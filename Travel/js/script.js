3802d642fac8cc76a012ba7948c2eaa22825fff0
Sermaxxx <55384895+Sermaxxx@users.noreply.github.com>
2019-10-18 15:18:46 +0500

    Add files via upload

---
+++ Travel/js/script.js
@@ -0,0 +1,25 @@
+var link = $('.nav-item a');
+link.on('click', function (e) {
+   e.preventDefault();
+   link.removeClass('active');
+   $(this).addClass('active');
+   var x = $(this).attr('href');
+   var y = $(x).offset().top
+   $('html').animate ({
+      scrollTop: y
+   }, 1000)
+})
+
+$(window).scroll(function () {
+   console.log($(this).scrollTop());
+   if ($(this).scrollTop() < 500) {
+       $("#top").fadeOut();        
+   } else {
+       $("#top").fadeIn();
+   }
+})
+$("#top").click(function () {
+   $("html").animate({
+       scrollTop: 0 
+   }, 500);
+})
